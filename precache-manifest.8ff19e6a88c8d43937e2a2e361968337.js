b1cee0729eeb260f72c13d7c828604ecfc2f712e
arpit-coder <arpitjain3105@gmail.com>
2022-11-18 15:51:15 +0530

    Updates

---
+++ precache-manifest.8ff19e6a88c8d43937e2a2e361968337.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "835d7b5eaa27d8b6beca0a8ae57f8d52",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "55ea01a810d513801580",
+    "url": "/react/static/css/2.5bba4796.chunk.css"
+  },
+  {
+    "revision": "30b4385bcec90ce10646",
+    "url": "/react/static/css/main.0df2efd5.chunk.css"
+  },
+  {
+    "revision": "55ea01a810d513801580",
+    "url": "/react/static/js/2.60d2a62a.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/react/static/js/2.60d2a62a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30b4385bcec90ce10646",
+    "url": "/react/static/js/main.7b52c120.chunk.js"
+  },
+  {
+    "revision": "8ae44a58772525c71de5",
+    "url": "/react/static/js/runtime-main.03d39bf8.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
